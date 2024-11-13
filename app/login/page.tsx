c6d55b42ff702df1bf3b1194de71b8c77d217232
Lucas Vasconcelos <lucasqueirozv12@gmail.com>
2024-11-13 16:32:38 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,27 @@
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { LogInIcon } from "lucide-react";
+import { redirect } from "next/dist/server/api-utils";
 import Image from "next/image";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/");
+  }
   return (
     <div className="grid h-full grid-cols-2">
-      {/* lado esquerdo da tela de login */}
+      {/* Lado esquerdo da tela de login */}
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
-        <Image
-          src="/Logo.svg"
-          width={173}
-          height={39}
-          alt="FinanceX"
-          className="mb-8"
-        />
+        <div className="mb-8 flex items-center">
+          <Image
+            src="/Logo.svg"
+            width={173}
+            height={39}
+            alt="FinanceX"
+            className="mr-4"
+          />
+        </div>
         <h1 className="mb-3 text-4xl font-bold">Bem-Vindo</h1>
         <p className="mb-8 text-muted-foreground">
           A FinanceX é uma plataforma de gerenciamento financeiro que aproveita
@@ -20,13 +29,17 @@ const LoginPage = () => {
           insights personalizados, tornando o controle do seu orçamento mais
           simples e eficiente.
         </p>
-        <button Variant="outline">
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </button>
+        <SignInButton>
+          <div className="flex items-center">
+            <button className="flex items-center space-x-2 rounded-md px-4 py-2 hover:bg-gray-100">
+              <LogInIcon />
+              <span>Faça login ou crie uma conta</span>
+            </button>
+          </div>
+        </SignInButton>
       </div>
       <div className="relative h-full w-full">
-        {/* lado direito da tela de login */}
+        {/* Lado direito da tela de login com imagem de fundo decorativa */}
         <Image
           src="/Decorative image.png"
           alt="Faça login"
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-const Home = () => {
-  return <h1>hello world</h1>;
+import { UserButton } from "@clerk/nextjs";
+import { button } from "./_components/ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/dist/server/api-utils";
+
+const Home = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("/login");
+  }
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
 };
 
 export default Home;
